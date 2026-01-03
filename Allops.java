cd7146b7d85782e48fb2d0b06da2e9fe3d6706cb
Yuvaaganesh K <yuvaaganeshk.08@gmail.com>
2026-01-03 16:17:05 +0530

    Add files via upload

---
+++ Allops.java
@@ -0,0 +1,11 @@
+public class Allops{
+public static void main(String[] arg){
+int x = 10;
+int y = 2;
+System.out.println(x+y);
+System.out.println(x-y);
+System.out.println(x*y);
+System.out.println(x/y);
+System.out.println(x%y);
+}
+}
