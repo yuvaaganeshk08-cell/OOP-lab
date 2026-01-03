c17a902191a9fcd40fd81ff17440dcbca7df34c5
Yuvaaganesh K <yuvaaganeshk.08@gmail.com>
2026-01-03 15:04:21 +0530

    Add files via upload

---
+++ AllDatatype.java
@@ -0,0 +1,9 @@
+public class AllDatatype{
+	public static void main(String[] args){
+		String name = "Yuvaaganesh k";
+		int Rollno = 53;
+		boolean is_student = true;
+		double gpa = 8.4;
+		System.out.print("My name is "+ name+" of roll number "+Rollno+".If you ask me, is am I a student, yes its "+is_student+".I scored gpa of" +gpa);
+}
+}
