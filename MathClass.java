f23f99101e16228c9629afc8b3a0b12a480ea047
Yuvaaganesh K <yuvaaganeshk.08@gmail.com>
2026-01-20 16:26:45 +0530

    Add files via upload

---
+++ MathClass.java
@@ -0,0 +1,23 @@
+public class MathClass{
+		public static void main(String[] args){
+
+			System.out.println(Math.PI);
+			System.out.println(Math.E);
+			double power = Math.pow(2,3);
+			System.out.println(power);			
+			double modulus = Math.abs(-7);
+			double sq_root = Math.sqrt(16);
+			double round = Math.round(36.5);
+			double ceil = Math.ceil(24.76);
+			double floor = Math.floor(24.76);
+			double max = Math.max(99,100);
+			double min = Math.min(12,33);
+			System.out.println(modulus);
+			System.out.println(sq_root);
+			System.out.println(round);
+			System.out.println(ceil);
+			System.out.println(floor);
+			System.out.println(max);	
+			System.out.println(min);
+			
+}}
