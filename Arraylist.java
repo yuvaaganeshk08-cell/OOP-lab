bd70bc3eb25209ccc51eb5c9af40e192f23989a9
Yuvaaganesh K <yuvaaganeshk.08@gmail.com>
2026-03-20 22:26:03 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Arraylist {
+    public static void main(String[] args){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(3);
+        list.add(7);
+        list.add(9);
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("apple");
+        fruits.add("orange");
+
+        System.out.println(fruits);
+
+        System.out.println(list);
+
+    }
+    
+}
