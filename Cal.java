a9efaff210bcd9d56b18b37883df365bbeafaa9a
Yuvaaganesh K <yuvaaganeshk.08@gmail.com>
2026-03-26 14:59:57 +0530

    Add files via upload

--- Cal.java
+++ Cal.java
@@ -1,8 +1,31 @@
-public class Cal{
-public static void main(String[] args){
-float a;
-a = 7/4*9/2;
-System.out.println(a);
 
+public class Cal{
+    public static void main(String[] args){
+        
+        Add x = new Add();
+        System.out.println(x.add(5,5));
+        System.out.println(x.add(7,4,2));   
+        System.out.println(x.GetAdd(5.6,3.4));
+
+    }
 }
+
+
+class Add{
+    
+    public int add(int a,int b){
+        return a+b;
+    }
+    
+    protected int add(int a,int b,int c){
+        return a+b+c;
+    }
+    
+    private double add( double a, double b){
+        return a+b;
+    }
+    public double GetAdd(double a, double b){
+        return add(a,b);
+    }
+    
 }
